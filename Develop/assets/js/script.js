814f3aead194fed0cbbb305e173d6f7506d0f38a
christi marchetti <christimarchetti@gmail.com>
2021-09-12 18:12:10 -0400

    else statement

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,82 +1,66 @@
-var uppercase
-var lowercase
-var numbers
-var symbols
-//password length
-var getPasswordLength = parseInt(prompt("Choose the number of charaters your password will contain."));
-if (getPasswordLength < 8) {
+//Arrays
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*()+[]{}";
+
+
+//Variables
+var length = "";
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumbers;
+var confirmSymbols;
+var characters = uppercase + lowercase + numbers + symbols;
+var passwordFinal
+
+var length = parseInt(prompt("Choose the number of charaters your password will contain.")); {
+if (length < 8) {
   alert("Password must contain a minimum of 8 charaters!")
-}
+  // var length = (prompt("Your password will contain (8-128) characters."));
+};
 
-if (getPasswordLength > 128) {
+if (length > 128) {
   alert("A maximum number of 128 characters can be used!")
-}
+};
 
-if (isNaN(getPasswordLength)) {
+if (isNaN(length)) 
   alert("Invalid password. Please input password.");
-  generatePassword();
-}
-
+  
 // password criteria
-var lowercase = window.confirm("Click OK to include lowercase letters.");
-if (lowercase === true){
-alert ('Your password will contain lowercase characters.')
+var confirmLowercase = window.confirm("Click OK to include lowercase letters.");
+if (confirmLowercase === true){
 } else {
-  alert('Your password will not contain lowercase characters.')
-}
-// };
-var uppercase = window.confirm("Click OK to include uppercase letters.");
-if (uppercase === true)
+  
+};
+}  
+var confirmUppercase = window.confirm("Click OK to include uppercase letters.");
+if (confirmUppercase === true){
+  alert ('Your password will contain uppercase characters.')
+  } else {
 
-var numbers = window.confirm("Click OK to include numbers.");
-if (numbers )
+  }
 
-var symbols = window.confirm("Click OK to include  symbols.");
+var confirmNumbers = window.confirm("Click OK to include numbers.");
+if (confirmNumbers === true){
+  alert ('Your password will contain numbers.')
+  } else {
+    
+  }
 
-if (!includeLowercase && !includeUppercase && !includeNumbers && includeSymbols)
-  alert("Minimum of 1 character type required!")
-
-window.addEventListener('load', function() {
-
-  generatePassword(); 
-});
-function generateNewPassword() {
-
-//   var password = "";
-
-//   var allowed = {};
-//   if (uppercase) password += random(allowed.uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-//   if (lowercase) password += random(allowed.lowercase= "abcdefghijklmnopqrstuvwxyz");
-//   if (numbers) password += random(allowed.numbers = "1234567890");
-//   if (symbols) password += random(allowed.symbols= "!@#$%^&*()+[]{},.");
-
-//   for (var i = password.length; i < length; i++) password += random(random(allowed).value);
-
-//   document.getElementById("password").value = Math.floor(Math.random()password.length).join("");
-
-// return result;
-
-// random lowercase
-  // function getRandomLower() {
-  //   return String.fromCharCode((Math.floor(Math.random() * 26) + );
-  // }
-
-  // // random uppercase
-  // function getRandomUpper() {
-  //   return String.fromCharCode((Math.floor(Math.random() * 26) + 65);
-  // }
-
-  // // random number
-  // function getRandomNumber() {
-  //   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  // }
-
-  // // random symbol
-  //   function getRandomSymbol() {
-  //   var symbols = '!@#$%^&*(){}=<>/,.';
-  //   return symbols[Math.floor(Math.random() * 26) + 97];
-  // }
-
+  var confirmSymbols = window.confirm("Click OK to include  symbols.");
+  if (confirmSymbols === true){
+    alert ('Your password will contain symbols.')
+    } else {
+  (!lowercase && !uppercase && !numbers && symbols)
+  alert('Minimum of 1 character type required!')
+    }
+function generatePassword() { 
+var passwordFinal = ""; 
+  for (var i = 0; i < length; i++) {
+  passwordFinal =+ characters[ Math.floor(Math.random() * characters.length)] + passwordFinal;
+  }
+  return passwordFinal();
 }
 
 // Get references to the #generate element
@@ -85,10 +69,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password")
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-
