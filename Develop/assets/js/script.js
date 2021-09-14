2c5214fabdf592f5d451d7adebc2e6ff8e4d0e89
christi marchetti <christimarchetti@gmail.com>
2021-09-13 21:03:14 -0400

    update

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -11,58 +11,70 @@ var confirmLowercase;
 var confirmUppercase;
 var confirmNumbers;
 var confirmSymbols;
-var characters = uppercase + lowercase + numbers + symbols;
+var characters = "";
 var passwordFinal
 
-var length = parseInt(prompt("Choose the number of charaters your password will contain.")); {
-if (length < 8) {
-  alert("Password must contain a minimum of 8 charaters!")
-  // var length = (prompt("Your password will contain (8-128) characters."));
-};
+function getLength() {
+  length = parseInt(prompt("Choose the number of charaters your password will contain.")); 
 
-if (length > 128) {
-  alert("A maximum number of 128 characters can be used!")
-};
+    if (length < 8) {
+      alert("Password must contain a minimum of 8 charaters!")
+      return getLength()
+      // var length = (prompt("Your password will contain (8-128) characters."));
+    };
 
-if (isNaN(length)) 
-  alert("Invalid password. Please input password.");
+    if (length > 128) {
+      alert("A maximum number of 128 characters can be used!")
+    };
+
+    if (isNaN(length)) {
+      alert("Invalid password. Please input password.");
+    }
+  }
+  function getCharacters() {
+    // password criteria
+    var confirmLowercase = window.confirm("Click OK to include lowercase letters.");
+    if (confirmLowercase === true) {
+      characters += lowercase
+    } else {
+
+    };
   
-// password criteria
-var confirmLowercase = window.confirm("Click OK to include lowercase letters.");
-if (confirmLowercase === true){
-} else {
-  
-};
-}  
-var confirmUppercase = window.confirm("Click OK to include uppercase letters.");
-if (confirmUppercase === true){
-  alert ('Your password will contain uppercase characters.')
+  var confirmUppercase = window.confirm("Click OK to include uppercase letters.");
+  if (confirmUppercase === true) {
+    characters += uppercase
+    alert('Your password will contain uppercase characters.')
   } else {
 
   }
 
-var confirmNumbers = window.confirm("Click OK to include numbers.");
-if (confirmNumbers === true){
-  alert ('Your password will contain numbers.')
+  var confirmNumbers = window.confirm("Click OK to include numbers.");
+  if (confirmNumbers === true) {
+    characters += numbers
+    alert('Your password will contain numbers.')
   } else {
-    
+
   }
 
   var confirmSymbols = window.confirm("Click OK to include  symbols.");
-  if (confirmSymbols === true){
-    alert ('Your password will contain symbols.')
-    } else {
-  (!lowercase && !uppercase && !numbers && symbols)
-  alert('Minimum of 1 character type required!')
-    }
-function generatePassword() { 
-var passwordFinal = ""; 
+  if (confirmSymbols === true) {
+    characters += symbols
+    alert('Your password will contain symbols.')
+  } 
+  if (!confirmLowercase && !confirmUppercase && !confirmNumbers && !confirmSymbols){
+    alert('Minimum of 1 character type required!');
+    return getCharacters()
+  }
+}
+function generatePassword() {
+  getLength()
+  getCharacters()
+  var passwordFinal = "";
   for (var i = 0; i < length; i++) {
-  passwordFinal =+ characters[ Math.floor(Math.random() * characters.length)] + passwordFinal;
+    passwordFinal += characters[Math.floor(Math.random() * characters.length)];
   }
   return passwordFinal;
 }
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
